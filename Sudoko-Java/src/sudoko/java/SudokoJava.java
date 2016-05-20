c8d0ffda52c3343c7334e9811e2a2705e93c643b
Jaswanth Bala <jaswanthc52@gmail.com>
2016-05-20 07:47:48 +0200

    Removed magic numbers

--- Sudoko-Java/src/sudoko/java/SudokoJava.java
+++ Sudoko-Java/src/sudoko/java/SudokoJava.java
@@ -5,6 +5,7 @@
  */
 package sudoko.java;
 
+import static java.lang.Math.sqrt;
 import java.util.Random;
 
 /**
@@ -12,14 +13,16 @@ import java.util.Random;
  * @author jaswanth
  */
 public class SudokoJava {
-
+    
+    static int gridSize = 4;
+    static int subGridSize = (int) sqrt(gridSize);
     static int sudokuGrid[][] = new int[][]
                                 {{0,0,0,0},
                                  {0,0,0,0},
                                  {0,0,0,0},
                                  {0,0,0,0}}; //Empty grid, where predefined 
                                              //values can be set.
-    static int grid[][] = new int[4][4]; //Grid to be manipulated by solver agent.
+    static int grid[][] = new int[gridSize][gridSize]; //Grid to be manipulated by solver agent.
     
     static final int firstBox = 0; //Index of first cell in the grid in a row.
     static final int lastBox = 3;  //Index of last cell in the grid in a row.
@@ -35,12 +38,13 @@ public class SudokoJava {
         
         sudokoGenerator(); //For generating the grid.
  
-        for(int i =0; i<4;i++){
-            for(int j=0;j<4;j++){
+        for(int i = firstBox; i < gridSize; i++){
+            for(int j = firstBox; j < gridSize; j++){
                 grid[i][j] = sudokuGrid[i][j];
             }
         }
-        displayGrid((sudokoSolverAgent(0, 0, grid))); //For solving the grid.
+        
+        displayGrid((sudokoSolverAgent(firstBox, firstBox, grid))); //For solving the grid.
     }
     
     public static void sudokoGenerator(){
@@ -48,20 +52,25 @@ public class SudokoJava {
         
         int predefinedRow = 0, predefinedCol = 0, predefinedNum = 0;
         int noPredefinedBoxes;
+        
         Random random = new Random();
+        
         noPredefinedBoxes = random.nextInt(maxNoPredefinedBoxes - 
                                 minNoPredefinedBoxes + 1) + minNoPredefinedBoxes;
-        for(int i=1; i<=noPredefinedBoxes; i++ ){
+        
+        for(int i = 1; i <= noPredefinedBoxes; i++ ){
             predefinedRow = random.nextInt(lastBox - firstBox + 1);
             predefinedCol = random.nextInt(lastBox - firstBox + 1);
             predefinedNum = random.nextInt(maxNumber - minNumber + 1) 
                             + minNumber;
             if(sudokuGrid[predefinedRow][predefinedCol] == 0){
                 sudokuGrid[predefinedRow][predefinedCol] = predefinedNum;
-                if(validMove(predefinedRow,predefinedCol,sudokuGrid)){
-                    continue;
-                }else{sudokuGrid[predefinedRow][predefinedCol] = 0;i--;}
-            } else {i--;}
+                if(validMove(predefinedRow,predefinedCol,sudokuGrid)) continue;
+                else {
+                    sudokuGrid[predefinedRow][predefinedCol] = 0;
+                    i--;
+                }
+            } else i--;
         }
         
         displayGrid(sudokuGrid);
@@ -70,27 +79,43 @@ public class SudokoJava {
     public static int[][] sudokoSolverAgent(int x, int y, int[][] grid){
         // Agent that solves the entire sudoko.
         
-        int validX = 3, validY = 3, flag = 0;
-        for(int i=3; i>=0; i--){
-            for(int j=3; j>=0; j--){
+        int validX = lastBox, validY = lastBox, flag = 0;
+        
+        for(int i = lastBox; i >= firstBox; i--){
+            for(int j = lastBox; j >= firstBox; j--){
                 if(sudokuGrid[i][j] == 0){
-                    validX = i; validY = j; flag = 1;
+                    validX = i; 
+                    validY = j; 
+                    flag = 1;
                     break;
                 }
             }
-            if(flag ==1){break;}
+            if(flag ==1) break;
         }
+        
         while((!validMove(validX,validY,grid) || grid[validX][validY] == 0)){
             if(sudokuGrid[x][y]!=0){
                 int newX,newY;
-                if(y==3){newX = x+1;newY=0;}else{newX=x; newY=y+1;}
+                if(y == lastBox){
+                    newX = x+1;
+                    newY = 0;
+                } else { 
+                    newX = x; 
+                    newY = y+1;
+                }
                 sudokoSolverAgent(newX,newY,grid);
-            }else{
-                if(grid[x][y] < 4) {
+            } else {
+                if(grid[x][y] < maxNumber) {
                     grid[x][y]++; //Incrementing the value in each cell by one.
                     if(validMove(x,y,grid)){
                         int newX,newY;
-                        if(y==3){newX = x+1;newY=0;}else{newX=x; newY=y+1;}
+                        if(y == lastBox){
+                            newX = x+1;
+                            newY = 0;
+                        } else {
+                            newX = x; 
+                            newY = y+1;
+                        }
                         sudokoSolverAgent(newX,newY,grid);
                     }
                 } else {
@@ -107,14 +132,15 @@ public class SudokoJava {
         // Method to check for valid moves.
         
         String cellValues = "";
-        for(int i=0;i<4;i++){
+        for(int i = 0; i < gridSize; i++){
             cellValues += Integer.toString(grid[i][y]); //Check vertical values.
             cellValues += Integer.toString(grid[x][i]); //Check horizontal values.
-            cellValues += Integer.toString(grid[(x/2)*2+i/2][(y/2)*2+i%2]); 
+            cellValues += Integer.toString(grid[(x/subGridSize) * subGridSize +
+                        i/subGridSize][(y/subGridSize) * subGridSize + i%subGridSize]); 
                                                     //Check values in the subgrid.
         }
         int count=0, flag=0;
-        while ((flag=cellValues.indexOf(Integer.toString(grid[x][y]), flag))!=-1){
+        while ((flag = cellValues.indexOf(Integer.toString(grid[x][y]), flag))!=-1){
             //Counting the number of occurences of the respective grid value.
             flag++;
             count++;
@@ -127,8 +153,8 @@ public class SudokoJava {
         // Method for displaying the grid.
         
         System.out.println();
-        for(int i =0; i<4;i++){
-            for(int j=0;j<4;j++){
+        for(int i = 0; i < gridSize; i++){
+            for(int j = 0;j < gridSize; j++){
                 System.out.print(grid[i][j] + "  ");
             }
             System.out.println();
